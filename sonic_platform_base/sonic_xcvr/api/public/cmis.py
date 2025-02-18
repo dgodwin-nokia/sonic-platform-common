8b7f267084e8efec753051720faa33f3f98309b9
dygodwin <dylan.godwin@nokia.com>
2025-02-18 15:01:30 -0500

    Wait for deinit/init success in decommission_all_datapaths

--- sonic_platform_base/sonic_xcvr/api/public/cmis.py
+++ sonic_platform_base/sonic_xcvr/api/public/cmis.py
@@ -2789,25 +2789,24 @@ class CmisApi(XcvrApi):
 
     def decommission_all_datapaths(self):
         '''
-            Return True if all datapaths are successfully de-commissioned, False otherwise
+        Return True if all datapaths are successfully de-commissioned, False otherwise
         '''
-        # De-init all datpaths
+        # De-init all datapaths
         self.set_datapath_deinit((1 << self.NUM_CHANNELS) - 1)
-        # Decommision all lanes by apply AppSel=0
-        self.set_application(((1 << self.NUM_CHANNELS) - 1), 0, 0)
-        # Start with AppSel=0 i.e undo any default AppSel
-        self.scs_apply_datapath_init((1 << self.NUM_CHANNELS) - 1)
-
-        dp_state = self.get_datapath_state()
-        config_state = self.get_config_datapath_hostlane_status()
-
+        DELAY_RETRY_SEC = 0.1
+        DELAY_MSEC = 1000
         for lane in range(self.NUM_CHANNELS):
             name = "DP{}State".format(lane + 1)
-            if dp_state[name] != 'DataPathDeactivated':
+            if not self.wait_time_condition(lambda: (self.get_datapath_state())[name], 'DataPathDeactivated', DELAY_MSEC, DELAY_RETRY_SEC):
                 return False
-            
+
+        # Decommission all lanes by applying AppSel=0
+        self.set_application(((1 << self.NUM_CHANNELS) - 1), 0, 0)
+        # Start with AppSel=0 i.e., undo any default AppSel
+        self.scs_apply_datapath_init((1 << self.NUM_CHANNELS) - 1)
+        for lane in range(self.NUM_CHANNELS):
             name = "ConfigStatusLane{}".format(lane + 1)
-            if config_state[name] != 'ConfigSuccess':
+            if not self.wait_time_condition(lambda: (self.get_config_datapath_hostlane_status())[name], 'ConfigSuccess', DELAY_MSEC, DELAY_RETRY_SEC):
                 return False
 
         return True
